253fa94d3f8015d2dffd934548813b10232336c6
Baldur Jónsson <baldur@helpdesk.is>
2023-10-06 08:51:08 +0000

    omnisharp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,14 +1,23 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = {"lua_ls", "csharp_ls"}
+	ensure_installed = {"lua_ls", "omnisharp"}
 })
 
 local lsp = require('lsp-zero').preset({})
-	
+
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+    local opts = {buffer = bufnr, remap = false}
+    lsp.default_keymaps({buffer = bufnr})
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 local cmp = require("cmp")
@@ -28,11 +37,14 @@ cmp.setup({
         fallback()
       end
     end, {"i","s","c",}),
-    ...
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
   }
 })
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').omnisharp.setup {
+    enable_import_completion    = true,
+}
 
 lsp.setup()
